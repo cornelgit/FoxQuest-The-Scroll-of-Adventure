545730ae03c5dfbeeb3280580be6728b3a0b4d8c
Cornel Stoica <cornel.stoica@oit.edu>
2024-09-17 18:17:08 -0700

    Added minor camera scrolling functionality

--- js/index.js
+++ js/index.js
@@ -1,171 +1,197 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-const dpr = window.devicePixelRatio || 1
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+const dpr = window.devicePixelRatio || 1;
 
-canvas.width = 1024 * dpr
-canvas.height = 576 * dpr
+canvas.width = 1024 * dpr;
+canvas.height = 576 * dpr;
 
 const layersData = {
-  l_New_Layer_1: l_New_Layer_1,
-  l_New_Layer_2: l_New_Layer_2,
-  l_New_Layer_8: l_New_Layer_8,
-  l_New_Layer_3: l_New_Layer_3,
-  l_Decorations: l_Decorations,
-  l_New_Layer_5: l_New_Layer_5,
-  l_New_Layer_6: l_New_Layer_6,
-  l_New_Layer_9: l_New_Layer_9,
-  l_New_Layer_9_1: l_New_Layer_9_1,
-}
+    l_New_Layer_1: l_New_Layer_1,
+    l_New_Layer_2: l_New_Layer_2,
+    l_New_Layer_8: l_New_Layer_8,
+    l_New_Layer_3: l_New_Layer_3,
+    l_Decorations: l_Decorations,
+    l_New_Layer_5: l_New_Layer_5,
+    l_New_Layer_6: l_New_Layer_6,
+    l_New_Layer_9: l_New_Layer_9,
+    l_New_Layer_9_1: l_New_Layer_9_1,
+};
 
 const tilesets = {
-  l_New_Layer_1: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_New_Layer_2: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_New_Layer_8: { imageUrl: './images/tileset.png', tileSize: 16 },
-  l_New_Layer_3: { imageUrl: './images/tileset.png', tileSize: 16 },
-  l_Decorations: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_New_Layer_5: { imageUrl: './images/tileset.png', tileSize: 16 },
-  l_New_Layer_6: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_New_Layer_9: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_New_Layer_9_1: { imageUrl: './images/tileset.png', tileSize: 16 },
-}
+    l_New_Layer_1: { imageUrl: "./images/decorations.png", tileSize: 16 },
+    l_New_Layer_2: { imageUrl: "./images/decorations.png", tileSize: 16 },
+    l_New_Layer_8: { imageUrl: "./images/tileset.png", tileSize: 16 },
+    l_New_Layer_3: { imageUrl: "./images/tileset.png", tileSize: 16 },
+    l_Decorations: { imageUrl: "./images/decorations.png", tileSize: 16 },
+    l_New_Layer_5: { imageUrl: "./images/tileset.png", tileSize: 16 },
+    l_New_Layer_6: { imageUrl: "./images/decorations.png", tileSize: 16 },
+    l_New_Layer_9: { imageUrl: "./images/decorations.png", tileSize: 16 },
+    l_New_Layer_9_1: { imageUrl: "./images/tileset.png", tileSize: 16 },
+};
 
 // Tile setup
-const collisionBlocks = []
-const platforms = []
-const blockSize = 16 // Assuming each tile is 16x16 pixels
+const collisionBlocks = [];
+const platforms = [];
+const blockSize = 16; // Assuming each tile is 16x16 pixels
 
 collisions.forEach((row, y) => {
-  row.forEach((symbol, x) => {
-    if (symbol === 1) {
-      collisionBlocks.push(
-        new CollisionBlock({
-          x: x * blockSize,
-          y: y * blockSize,
-          size: blockSize,
-        }),
-      )
-    } else if (symbol === 2) {
-      platforms.push(
-        new Platform({
-          x: x * blockSize,
-          y: y * blockSize + blockSize,
-          width: 16,
-          height: 4,
-        }),
-      )
-    }
-  })
-})
+    row.forEach((symbol, x) => {
+        if (symbol === 1) {
+            collisionBlocks.push(
+                new CollisionBlock({
+                    x: x * blockSize,
+                    y: y * blockSize,
+                    size: blockSize,
+                })
+            );
+        } else if (symbol === 2) {
+            platforms.push(
+                new Platform({
+                    x: x * blockSize,
+                    y: y * blockSize + blockSize,
+                    width: 16,
+                    height: 4,
+                })
+            );
+        }
+    });
+});
 
 const renderLayer = (tilesData, tilesetImage, tileSize, context) => {
-  tilesData.forEach((row, y) => {
-    row.forEach((symbol, x) => {
-      if (symbol !== 0) {
-        const srcX = ((symbol - 1) % (tilesetImage.width / tileSize)) * tileSize
-        const srcY =
-          Math.floor((symbol - 1) / (tilesetImage.width / tileSize)) * tileSize
+    tilesData.forEach((row, y) => {
+        row.forEach((symbol, x) => {
+            if (symbol !== 0) {
+                const srcX =
+                    ((symbol - 1) % (tilesetImage.width / tileSize)) * tileSize;
+                const srcY =
+                    Math.floor((symbol - 1) / (tilesetImage.width / tileSize)) *
+                    tileSize;
 
-        context.drawImage(
-          tilesetImage, // source image
-          srcX,
-          srcY, // source x, y
-          tileSize,
-          tileSize, // source width, height
-          x * 16,
-          y * 16, // destination x, y
-          16,
-          16, // destination width, height
-        )
-      }
-    })
-  })
-}
+                context.drawImage(
+                    tilesetImage, // source image
+                    srcX,
+                    srcY, // source x, y
+                    tileSize,
+                    tileSize, // source width, height
+                    x * 16,
+                    y * 16, // destination x, y
+                    16,
+                    16 // destination width, height
+                );
+            }
+        });
+    });
+};
 
 const renderStaticLayers = async () => {
-  const offscreenCanvas = document.createElement('canvas')
-  offscreenCanvas.width = canvas.width
-  offscreenCanvas.height = canvas.height
-  const offscreenContext = offscreenCanvas.getContext('2d')
+    const offscreenCanvas = document.createElement("canvas");
+    offscreenCanvas.width = canvas.width;
+    offscreenCanvas.height = canvas.height;
+    const offscreenContext = offscreenCanvas.getContext("2d");
 
-  for (const [layerName, tilesData] of Object.entries(layersData)) {
-    const tilesetInfo = tilesets[layerName]
-    if (tilesetInfo) {
-      try {
-        const tilesetImage = await loadImage(tilesetInfo.imageUrl)
-        renderLayer(
-          tilesData,
-          tilesetImage,
-          tilesetInfo.tileSize,
-          offscreenContext,
-        )
-      } catch (error) {
-        console.error(`Failed to load image for layer ${layerName}:`, error)
-      }
+    for (const [layerName, tilesData] of Object.entries(layersData)) {
+        const tilesetInfo = tilesets[layerName];
+        if (tilesetInfo) {
+            try {
+                const tilesetImage = await loadImage(tilesetInfo.imageUrl);
+                renderLayer(
+                    tilesData,
+                    tilesetImage,
+                    tilesetInfo.tileSize,
+                    offscreenContext
+                );
+            } catch (error) {
+                console.error(
+                    `Failed to load image for layer ${layerName}:`,
+                    error
+                );
+            }
+        }
     }
-  }
 
-  // Optionally draw collision blocks and platforms for debugging
-  // collisionBlocks.forEach(block => block.draw(offscreenContext));
-  // platforms.forEach((platform) => platform.draw(offscreenContext))
+    // Optionally draw collision blocks and platforms for debugging
+    // collisionBlocks.forEach(block => block.draw(offscreenContext));
+    // platforms.forEach((platform) => platform.draw(offscreenContext))
 
-  return offscreenCanvas
-}
+    return offscreenCanvas;
+};
 // END - Tile setup
 
 // Change xy coordinates to move player's default position
 const player = new Player({
-  x: 100,
-  y: 100,
-  size: 16,
-  velocity: { x: 0, y: 0 },
-})
+    x: 100,
+    y: 100,
+    size: 16,
+    velocity: { x: 0, y: 0 },
+});
 
 const keys = {
-  w: {
-    pressed: false,
-  },
-  a: {
-    pressed: false,
-  },
-  d: {
-    pressed: false,
-  },
-}
+    w: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+};
 
-let lastTime = performance.now()
+let lastTime = performance.now();
+const camera = {
+    x: 0,
+    y: 0,
+};
+
+const SCROLL_POST_RIGHT = 500;
+const SCROLL_POST_TOP = 200;
+
 function animate(backgroundCanvas) {
-  // Calculate delta time
-  const currentTime = performance.now()
-  const deltaTime = (currentTime - lastTime) / 1000
-  lastTime = currentTime
+    // Calculate delta time
+    const currentTime = performance.now();
+    const deltaTime = (currentTime - lastTime) / 1000;
+    lastTime = currentTime;
 
-  // Update player position
-  player.handleInput(keys)
-  player.update(deltaTime, collisionBlocks)
+    // Update player position
+    player.handleInput(keys);
+    player.update(deltaTime, collisionBlocks);
 
-  // Render scene
-  c.save()
-  c.scale(dpr, dpr)
-  c.clearRect(0, 0, canvas.width, canvas.height)
-  c.drawImage(backgroundCanvas, 0, 0)
-  player.draw(c)
-  c.restore()
+    // Track scroll post distance
+    if (player.x > SCROLL_POST_RIGHT) {
+        const scrollPostDistance = player.x - SCROLL_POST_RIGHT;
+        camera.x = scrollPostDistance;
+    }
+    if (player.y < SCROLL_POST_TOP) {
+        const scrollPostDistance = player.y - SCROLL_POST_TOP;
+        camera.y = scrollPostDistance;
+    }
 
-  requestAnimationFrame(() => animate(backgroundCanvas))
+    // Render scene
+    c.save();
+    c.scale(dpr, dpr);
+    c.translate(-camera.x, camera.y);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.drawImage(backgroundCanvas, 0, 0);
+    player.draw(c);
+    c.fillRect(SCROLL_POST_RIGHT, 100, 10, 100);
+    c.fillRect(300, SCROLL_POST_TOP, 100, 10);
+    c.restore();
+
+    requestAnimationFrame(() => animate(backgroundCanvas));
 }
 
 const startRendering = async () => {
-  try {
-    const backgroundCanvas = await renderStaticLayers()
-    if (!backgroundCanvas) {
-      console.error('Failed to create the background canvas')
-      return
+    try {
+        const backgroundCanvas = await renderStaticLayers();
+        if (!backgroundCanvas) {
+            console.error("Failed to create the background canvas");
+            return;
+        }
+
+        animate(backgroundCanvas);
+    } catch (error) {
+        console.error("Error during rendering:", error);
     }
+};
 
-    animate(backgroundCanvas)
-  } catch (error) {
-    console.error('Error during rendering:', error)
-  }
-}
-
-startRendering()
+startRendering();
