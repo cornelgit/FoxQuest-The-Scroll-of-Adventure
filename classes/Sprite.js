1b013ef1b4c22fda938d7aa7953083f8c1fd2132
Cornel Stoica <cornel.stoica@oit.edu>
2024-09-22 20:48:41 -0700

    Implemented gem collection logic as well as gem count tracker

--- classes/Sprite.js
+++ classes/Sprite.js
@@ -12,6 +12,12 @@ class Sprite {
             height: 28,
             frames: 6,
         },
+        hitbox = {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+        },
     }) {
         this.x = x;
         this.y = y;
@@ -27,22 +33,10 @@ class Sprite {
         this.currentFrame = 0;
         this.currentSprite = spriteCropbox;
         this.iteration = 0;
+        this.hitbox = hitbox;
     }
 
     draw(c) {
-        // Red square debug code
-        // c.fillStyle = 'rgba(255, 0, 0, 0.5)'
-        // c.fillRect(this.x, this.y, this.width, this.height)
-
-        // Hitbox
-        // c.fillStyle = 'rgba(0, 0, 255, 0.5)'
-        // c.fillRect(
-        //   this.hitbox.x,
-        //   this.hitbox.y,
-        //   this.hitbox.width,
-        //   this.hitbox.height,
-        // )
-
         if (this.isImageLoaded === true) {
             let xScale = 1;
             let x = this.x;
